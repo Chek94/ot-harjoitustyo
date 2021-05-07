9c983903e558c9aef6306f9464bf7e864dbe0471
Mikael Salopelto <mikael.salopelto@helsinki.fi>
2021-05-07 10:14:52 +0300

    basic ui elements

---
+++ chess/src/main/java/com/mycompany/chess/State.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.chess;
+
+/**
+ *
+ * @author misalope
+ */
+public class State {
+    
+    char[][] board;
+    
+}
